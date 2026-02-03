1d25cd02ed16e978b203f52d5bb6d23fd3c3bce9
solidrobo <maddra@gmail.com>
2026-02-03 13:37:29 +0200

    adds back sequence nr

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ from time import sleep
 import socket
 
 FILENAME = 'recording-20260203130920.json'
+
 IP = "2.255.255.255"
 PORT = 6454
 FRAME_RATE = 1/40
@@ -14,12 +15,13 @@ with open(FILENAME, 'r') as f:
 
 frames = []
 frame = None
-counter = 0
+sequence_nr = 0
 for packet in data:
   if packet['opcode'] == 82:
     if frame and len(frame):
       frames.append(frame)
     frame = []
+    sequence_nr = sequence_nr % 255 + 1
   else:
     if frame is None:
       continue
@@ -27,15 +29,13 @@ for packet in data:
     id = b64decode(packet['id'])
     opcode = packet['opcode']
     version = packet['version']
-    #counter = counter % 255 + 1
-    counter = 0
     physical = packet['physical']
     sub = packet['sub']
     net = packet['net']
     length = packet['length']
     dmx = b64decode(packet['dmx'])
     msg = pack(f'!8sHHBBBBH{length}s',
-              id, opcode, version, counter, physical, sub, net, length, dmx)
+              id, opcode, version, sequence_nr, physical, sub, net, length, dmx)
     frame.append(msg)
 
 try:
